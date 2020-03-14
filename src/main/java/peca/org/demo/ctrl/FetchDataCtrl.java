22a5faeab8eb4c215c201f30ff7a30fcbe796ac8
Petar Poznanovic <47084459+ppoznanovic17@users.noreply.github.com>
2020-03-14 04:43:21 +0100

    Update FetchDataCtrl.java

--- src/main/java/peca/org/demo/ctrl/FetchDataCtrl.java
+++ src/main/java/peca/org/demo/ctrl/FetchDataCtrl.java
@@ -12,6 +12,7 @@ import java.util.Collections;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class FetchDataCtrl {
 
